cb8161b54a4652d8bfb6993462f66445b04a7437
Mathieu Barbin <mathieu.barbin@gmail.com>
2025-09-21 14:04:04 +0200

    Switch to pplumbing split pkgs

--- example/bin/main.ml
+++ example/bin/main.ml
@@ -5,7 +5,7 @@
 (*********************************************************************************)
 
 let () =
-  Cmdlang_cmdliner_runner.run
+  Cmdlang_cmdliner_err_runner.run
     Dune_rpc_eio_example.main
     ~name:"main"
     ~version:"%%VERSION%%"
